587ff8eec9bbe36189168d725b669a21978d3d6a
juliocesar101290 <juliocesarleyvarodriguez@hotmail.com>
2025-07-04 11:31:20 -0700

    Corrections for dashboard Sells BranchOffice per  user

--- src/Models/BranchofficesModel.php
+++ src/Models/BranchofficesModel.php
@@ -4,62 +4,57 @@ namespace julio101290\boilerplatebranchoffice\Models;
 
 use CodeIgniter\Model;
 
-class BranchofficesModel extends Model{
+class BranchofficesModel extends Model {
 
-    protected $table      = 'branchoffices';
+    protected $table = 'branchoffices';
     protected $primaryKey = 'id';
     protected $useAutoIncrement = true;
-    protected $returnType     = 'array';
+    protected $returnType = 'array';
     protected $useSoftDeletes = true;
     protected $allowedFields = ['key'
-    ,'name'
-    ,'cologne'
-    ,'city'
-    ,'postalCode'
-    ,'timeDifference'
-    ,'tax'
-    ,'dateAp'
-    ,'phone'
-    ,'fax'
-    ,'companie'
-    ,'arqueoCaja'
-    ,'created_at
-    ','deleted_at'
-    ,'updated_at'];
+        , 'name'
+        , 'cologne'
+        , 'city'
+        , 'postalCode'
+        , 'timeDifference'
+        , 'tax'
+        , 'dateAp'
+        , 'phone'
+        , 'fax'
+        , 'companie'
+        , 'arqueoCaja'
+        , 'created_at
+    ', 'deleted_at'
+        , 'updated_at'];
     protected $useTimestamps = true;
-    protected $createdField  = 'created_at';
-    protected $deletedField  = 'deleted_at';
-
+    protected $createdField = 'created_at';
+    protected $deletedField = 'deleted_at';
     protected $validationRules = [
-        'key'           => 'required|alpha_numeric|max_length[8]',
-        'name'          => 'permit_empty|string|max_length[256]',
-        'cologne'       => 'permit_empty|string|max_length[64]',
-        'city'          => 'permit_empty|string|max_length[128]',
-        'postalCode'    => 'permit_empty|numeric|exact_length[5]',
-        'timeDifference'=> 'permit_empty|string|max_length[4]',
-        'tax'           => 'permit_empty|string|max_length[4]',
-        'dateAp'        => 'valid_date',
-        'phone'         => 'permit_empty|string|max_length[16]',
-        'fax'           => 'permit_empty|string|max_length[16]',
-        'companie'      => 'permit_empty|alpha_numeric|max_length[8]',
-        'arqueoCaja'    => 'permit_empty|string|max_length[5]',
+        'key' => 'required|alpha_numeric|max_length[8]',
+        'name' => 'permit_empty|string|max_length[256]',
+        'cologne' => 'permit_empty|string|max_length[64]',
+        'city' => 'permit_empty|string|max_length[128]',
+        'postalCode' => 'permit_empty|numeric|exact_length[5]',
+        'timeDifference' => 'permit_empty|string|max_length[4]',
+        'tax' => 'permit_empty|string|max_length[4]',
+        'dateAp' => 'valid_date',
+        'phone' => 'permit_empty|string|max_length[16]',
+        'fax' => 'permit_empty|string|max_length[16]',
+        'companie' => 'permit_empty|alpha_numeric|max_length[8]',
+        'arqueoCaja' => 'permit_empty|string|max_length[5]',
     ];
     protected $validationMessages = [];
-    protected $skipValidation     = false;
-    
-    
-        public function mdlSucursalesPorUsuario($usuario){
+    protected $skipValidation = false;
 
-
-        $resultado =$this->db->table('branchoffices a, usuarios_sucursal b')
-        ->select('a.id,a.name,key,a.created_at,a.updated_at,a.deleted_at')
-        ->where('a.id', 'b.idSucursal', FALSE)
-        ->where('b.status', 'on')
-        ->where('b.idUsuario', $usuario)->get()->getResultArray();
+    public function mdlSucursalesPorUsuario($usuario) {
+        $resultado = $this->db->table('branchoffices a')
+                ->select('a.id, a.name, a.key, a.created_at, a.updated_at, a.deleted_at')
+                ->join('usuarios_sucursal b', 'a.id = b.idSucursal')
+                ->where('b.status', 'on')
+                ->where('b.idUsuario', $usuario)
+                ->get()
+                ->getResultArray();
 
         return $resultado;
-
     }
-
 }
-        
